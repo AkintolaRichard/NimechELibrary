1c4b098c366f60eb7855859d483b965745372fd5
AkintolaRichard <princerichard17a@gmail.com>
2023-12-02 21:35:07 +0100

    fix: Add change to settings file * Call load_dotenv function

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import os
-load_dotenv
+load_dotenv()
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
-ALGORITH = os.environ.get("ALGORITH")
+ALGORITHM = os.environ.get("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
 DB_URL = os.environ.get("DB_URL")
