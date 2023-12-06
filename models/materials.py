3644d0b96df4143eeac7d445d0d2e449d20302e7
AkintolaRichard <princerichard17a@gmail.com>
2023-12-06 19:23:38 +0100

    chore: Add new attributes to Material class * Add Enum, year

--- models/materials.py
+++ models/materials.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel
+from enum import Enum
+
+class Year(str, Enum):
+    year_1 = "1"
+    year_2 = "2"
+    year_3 = "3"
+    year_4 = "4"
+    year_5 = "5"
 
 class Material(BaseModel):
     courseCode: str
@@ -6,6 +14,7 @@ class Material(BaseModel):
     courseLecturer: str
     materialName: str
     url: str
+    year: Year
 
 class Search(BaseModel):
     searchword: str
